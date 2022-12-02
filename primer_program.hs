6dcfeb90e7241cd471e5d6379d3619b3bca46833
clon54programer <112908720+clon54programer@users.noreply.github.com>
2022-12-01 23:03:35 -0500

    first commit

---
+++ primer_program.hs
@@ -0,0 +1,32 @@
+--Para salir del enterprete de haskell es Ctr+D
+tuCumpleaños = 1987
+
+tuNombre = "lucia"
+
+doble x = 2 * x
+
+dobledexMasuno x = (doble x) + 1
+-- :l primer_program.hs haci se ejecuta un programa de haskell
+-- se debe colocar el nombre de la funcion para ejecutar el codigo
+
+tuFecha = "juandanilo2007"
+
+divicion y = y / 2
+
+resta e = 100 - e
+
+suma w = 23 + w
+
+nombre = 12
+
+verde = 12
+
+promedio s t h ñ = (s+t+h+ñ) / 3 --como sacr el promedio
+
+valdeAgua z q p = (z+q+p) / 2
+
+broma = "eres el tecer bobo que escribe esto"
+
+volumenEsfera r = (4/3)*pi*r^2--pi es un contante
+
+veReme v = v+pi
